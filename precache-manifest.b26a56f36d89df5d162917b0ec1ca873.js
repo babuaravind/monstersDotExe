f121d814ae0d5cb26a30ae622e0c7ecbcc4e5473
babuaravind <itsbabuaravind@gmail.com>
2019-08-28 19:04:18 +0530

    Updates

---
+++ precache-manifest.b26a56f36d89df5d162917b0ec1ca873.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa70bb7ab1b9c99ce7a6e679595cf63",
+    "url": "/monstersdotexe/index.html"
+  },
+  {
+    "revision": "3badae3e073db67c1ecd",
+    "url": "/monstersdotexe/static/css/main.735be0ae.chunk.css"
+  },
+  {
+    "revision": "a9690e2a70e33c66e568",
+    "url": "/monstersdotexe/static/js/2.3df813ea.chunk.js"
+  },
+  {
+    "revision": "3badae3e073db67c1ecd",
+    "url": "/monstersdotexe/static/js/main.593a4104.chunk.js"
+  },
+  {
+    "revision": "649f909b11016b710cad",
+    "url": "/monstersdotexe/static/js/runtime~main.b5c13a9b.js"
+  }
+]);
